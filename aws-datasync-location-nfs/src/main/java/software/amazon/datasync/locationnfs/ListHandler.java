3a120ea3ef174b8dd638ef6f662f4ef0b2f4c6b7
Jason Posner <posnerjp@amazon.com>
2020-07-15 19:47:18 +0000

    Location NFS handlers and Unit Tests

---
+++ aws-datasync-location-nfs/src/main/java/software/amazon/datasync/locationnfs/ListHandler.java
@@ -0,0 +1,62 @@
+package software.amazon.datasync.locationnfs;
+
+import software.amazon.awssdk.services.datasync.DataSyncClient;
+import software.amazon.awssdk.services.datasync.model.DataSyncException;
+import software.amazon.awssdk.services.datasync.model.InternalException;
+import software.amazon.awssdk.services.datasync.model.InvalidRequestException;
+import software.amazon.awssdk.services.datasync.model.ListLocationsRequest;
+import software.amazon.awssdk.services.datasync.model.ListLocationsResponse;
+import software.amazon.awssdk.services.datasync.model.LocationListEntry;
+import software.amazon.cloudformation.exceptions.CfnGeneralServiceException;
+import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
+import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListHandler extends BaseHandler<CallbackContext> {
+
+    @Override
+    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
+
+        final String nextToken = request.getNextToken();
+        final DataSyncClient client = ClientBuilder.getClient();
+
+        final ListLocationsRequest listLocationsRequest = Translator.translateToListRequest(nextToken);
+
+        final ListLocationsResponse response;
+        try {
+            response = proxy.injectCredentialsAndInvokeV2(listLocationsRequest, client::listLocations);
+        } catch (InvalidRequestException e) {
+            throw new CfnInvalidRequestException(listLocationsRequest.toString(), e.getCause());
+        } catch (InternalException e) {
+            throw new CfnServiceInternalErrorException(listLocationsRequest.toString(), e.getCause());
+        } catch (DataSyncException e) {
+            throw new CfnGeneralServiceException(listLocationsRequest.toString(), e.getCause());
+        }
+
+        final List<ResourceModel> models = new ArrayList<>();
+        for (LocationListEntry loc : response.locations()) {
+            ResourceModel model = ResourceModel.builder()
+                    .locationArn(loc.locationArn())
+                    .locationUri(loc.locationUri())
+                    .build();
+            models.add(model);
+        }
+
+        return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                .resourceModels(models)
+                .status(OperationStatus.SUCCESS)
+                .nextToken(response.nextToken())
+                .build();
+    }
+}
---
+++ aws-datasync-location-nfs/src/main/java/software/amazon/datasync/locationnfs/ReadHandler.java
@@ -0,0 +1,65 @@
+package software.amazon.datasync.locationnfs;
+
+import software.amazon.awssdk.services.datasync.DataSyncClient;
+import software.amazon.awssdk.services.datasync.model.DataSyncException;
+import software.amazon.awssdk.services.datasync.model.DescribeLocationNfsRequest;
+import software.amazon.awssdk.services.datasync.model.DescribeLocationNfsResponse;
+import software.amazon.awssdk.services.datasync.model.InternalException;
+import software.amazon.awssdk.services.datasync.model.InvalidRequestException;
+import software.amazon.cloudformation.exceptions.CfnGeneralServiceException;
+import software.amazon.cloudformation.exceptions.CfnNotFoundException;
+import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+public class ReadHandler extends BaseHandler<CallbackContext> {
+
+    @Override
+    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final Logger logger) {
+
+        final ResourceModel model = request.getDesiredResourceState();
+        final DataSyncClient client = ClientBuilder.getClient();
+
+        final DescribeLocationNfsRequest describeLocationNfsRequest =
+                Translator.translateToReadRequest(model.getLocationArn());
+
+        DescribeLocationNfsResponse response;
+        try {
+            response = proxy.injectCredentialsAndInvokeV2(describeLocationNfsRequest, client::describeLocationNfs);
+        } catch (InvalidRequestException e) {
+            throw new CfnNotFoundException(ResourceModel.TYPE_NAME, model.getLocationArn());
+        } catch (InternalException e) {
+            throw new CfnServiceInternalErrorException(describeLocationNfsRequest.toString(), e.getCause());
+        } catch (DataSyncException e) {
+            throw new CfnGeneralServiceException(describeLocationNfsRequest.toString(), e.getCause());
+        }
+
+        if (response == null)
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .resourceModel(model)
+                    .status(OperationStatus.SUCCESS)
+                    .build();
+
+        ResourceModel returnModel = ResourceModel.builder()
+                .locationArn(response.locationArn())
+                .locationUri(response.locationUri())
+                .mountOptions(Translator.translateToResourceModelMountOptions(response.mountOptions()))
+                .onPremConfig(Translator.translateToResourceModelOnPremConfig(response.onPremConfig()))
+                .subdirectory(model.getSubdirectory())
+                .serverHostname(model.getServerHostname())
+                .tags(model.getTags())
+                .build();
+
+        return ProgressEvent.<ResourceModel, CallbackContext>builder()
+            .resourceModel(returnModel)
+            .status(OperationStatus.SUCCESS)
+            .build();
+    }
+}
